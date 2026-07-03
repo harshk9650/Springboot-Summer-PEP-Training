8f08b7a9a8afad59bb7f1f7cd4d3d38433e4305d
Harsh Kumar <149566587+harshk9650@users.noreply.github.com>
2026-07-03 12:48:05 +0530

    Add files via upload

---
+++ Spring(lombok)2july/src/main/java/com/example/demo/model/Employee.java
@@ -0,0 +1,20 @@
+package com.example.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+	public class Employee {
+		private int id;
+		private String name;
+		private int age;
+		private int salary;
+		private String desig;
+		
+
+}
